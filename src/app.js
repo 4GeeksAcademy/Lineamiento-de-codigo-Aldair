1b3110c6bc9d3a33fcf8521dc085966ba2d4ed33
AldairMoralesAlfaro <ald_brake@hotmail.com>
2025-06-29 04:56:22 +0000

    se arrega codigo del generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['tu', 'yo', 'nosotros', 'ellos'];
+let subadjetivo = ['encontre', 'tenemos', 'buscandoun', 'teniendo'];
+let adjetivo = ['chico', 'grande', 'muygrande', 'muypequeño',];
+let descripcion = ['bonito', 'feo', 'bonita', 'fea'];
+let extensionesDeDominio = ['.com', '.es', '.br', '.ch', '.eu', '.mx'];
 
+const combinacionesDeDominio = () => {
+  for (let index = 0; index < pronoun.length; index++) {
+    const subjec = pronoun[index];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    for (let indexSubadjetivo = 0; indexSubadjetivo < subadjetivo.length; indexSubadjetivo++) {
+      const subadjetivo2 = subadjetivo[indexSubadjetivo];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+      for (let indexAdjetivo = 0; indexAdjetivo < adjetivo.length; indexAdjetivo++) {
+        const element = adjetivo[indexAdjetivo];
+
+        for (let indexDescripcion = 0; indexDescripcion < descripcion.length; indexDescripcion++) {
+          const descripcionDeElemento = descripcion[indexDescripcion];
+
+          for (let indexExtensionesDeDominio = 0; indexExtensionesDeDominio < extensionesDeDominio.length; indexExtensionesDeDominio++) {
+            const DextensionesDeDominio = extensionesDeDominio[indexExtensionesDeDominio];
+
+            console.log(subjec + subadjetivo2 + element + descripcionDeElemento + DextensionesDeDominio);
+          }
+        }
+      }
+    }
+  }
+}
+combinaciones();
